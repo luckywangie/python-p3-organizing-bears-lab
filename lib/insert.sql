167bd02aafb0b99463eac4c3a9e44edc742d7012
lucky wangie <wangiemamati@gmail.com>
2025-02-27 14:53:19 +0300

    tests pass

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 6, 'F', 'Golden', 'Playful', 1),
+    ('Sergeant Brown', 8, 'M', 'Brown', 'Serious', 1),
+    ('Melissa', 7, 'F', 'Black', 'Gentle', 1),
+    ('Grinch', 4, 'M', 'Greenish', 'Grumpy', 1),
+    ('Wendy', 3, 'F', 'White', 'Friendly', 1),
+    (NULL, 2, 'M', 'Gray', 'Unknown', 1);
